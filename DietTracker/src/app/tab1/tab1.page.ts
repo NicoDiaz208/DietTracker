90fd69eb0135c699631ddcc6a37a41103ab21534
DESKTOP-IBPR654\User <tobias@fraunberger.co.at>
2021-01-22 11:11:57 +0100

    Erfolge Frontend

--- DietTracker/src/app/tab1/tab1.page.ts
+++ DietTracker/src/app/tab1/tab1.page.ts
@@ -6,7 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  private watercount:number = 0;
+  private sleepcount: number = 0;
+  constructor() {
+    
+  }
 
-  constructor() {}
-
+  public plus(){
+    this.watercount = this.watercount + 1;
+  }
+  public minus(){
+    this.watercount = this.watercount - 1;
+    if(this.watercount < 0){
+      this.watercount = 0;
+    }
+  }
+  public plusS(){
+    this.sleepcount = this.sleepcount + 1;
+  }
+  public minusS(){
+    this.sleepcount = this.sleepcount - 1;
+    if(this.sleepcount < 0){
+      this.sleepcount = 0;
+    }
+  }
 }
+

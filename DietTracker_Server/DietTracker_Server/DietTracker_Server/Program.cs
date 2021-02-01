0611f6f3ba0b71a3e16f0d6799061305944cb527
Amel-Dzogovic <lordamel@yahoo.com>
2021-02-01 15:11:22 +0100

    Datenbank Connection

---
+++ DietTracker_Server/DietTracker_Server/DietTracker_Server/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace DietTracker_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public class MongoCRUD
+        {
+            private IMongoDatabase db;
+
+            public MongoCRUD(string database)
+            {
+                var client = new MongoClient("mongodb://localhost:27017");
+                db = client.GetDatabase(database);
+            }
+            public void insertRecord<T>(string table, T record)
+            {
+                var collection = db.GetCollection<T>(table);
+                collection.InsertOne(record);
+            }
+        }
+    }
+}

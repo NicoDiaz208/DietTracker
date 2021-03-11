d8db2ee0b19dc547339a1d28e0664c7d7a46d202
StonewallR <ameldzogovic0@gmail.com>
2021-03-11 11:42:52 +0100

    records erstellt

--- DietTracker_Server/DietTracker_Server/DietTracker_Server/Classes/WaterIntake/WaterIntake.cs
+++ DietTracker_Server/DietTracker_Server/DietTracker_Server/Classes/WaterIntake/WaterIntake.cs
@@ -4,17 +4,6 @@ using System.Text;
 
 namespace DietTracker_Server.Classes.WaterIntake
 {
-    class WaterIntake
-    {
-        //GoWG=Glasses of Water Goal
-        public int GoWG { get; set; }
-        //GoWC=Glasses of Water Current
-        public int GoWC { get; set; }
-
-        public WaterIntake(int goWG, int goWC)
-        {
-            GoWG = goWG;
-            GoWC = goWC;
-        }
-    }
+    public record WaterIntake(int GoWG, int GoWC);
+    
 }

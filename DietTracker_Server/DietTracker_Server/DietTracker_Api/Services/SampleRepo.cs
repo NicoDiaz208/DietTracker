6769505d07c908c2d01f85a3df8c8bf6e829bcde
NicoDiaz208 <nicodiaz208@gmail.com>
2021-03-13 11:06:30 +0100

    Diettracker_api

---
+++ DietTracker_Server/DietTracker_Server/DietTracker_Api/Services/SampleRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DietTracker_Api.Services
+{
+
+    public record Sample(string name);
+
+    public class SampleRepo : ISampleRepo
+    {
+        private List<Sample> samples { get; } = new();
+        public Sample Add(Sample sample)
+        {
+            samples.Add(sample);
+            return sample;
+        }
+
+        public IEnumerable<Sample> GetAll() => samples;
+
+    }
+}

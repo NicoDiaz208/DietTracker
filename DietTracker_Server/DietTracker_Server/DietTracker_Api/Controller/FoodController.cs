68a242cf59686005dc935bfe4e945c1452722ebb
NicoDiaz208 <nicodiaz208@gmail.com>
2021-04-28 17:54:09 +0200

    FoodController added

---
+++ DietTracker_Server/DietTracker_Server/DietTracker_Api/Controller/FoodController.cs
@@ -0,0 +1,64 @@
+﻿using DietTracker_DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DietTracker_Api.Controller
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        private readonly IMongoCollection<Food> foodCollection;
+
+        public FoodController(CollectionFactory cf)
+        {
+            foodCollection = cf.GetCollection<Food>();
+        }
+
+        public record FoodCreationDto(
+            string name
+            );
+
+        public record FoodDto(
+            string id,
+            string name
+            );
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Food>>> GetAll()
+        {
+            var dbResult = await foodCollection.GetAll();
+            var result = dbResult.Select(a => new FoodDto(a.Id.ToString(), a.Name));
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}", Name = nameof(GetSingleFood))]
+        public async Task<ActionResult<FoodDto>> GetSingleFood(string id)
+        {
+            var result = await foodCollection.GetById(id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return new FoodDto(result.Id.ToString(), result.Name);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<FoodDto>> Add(FoodCreationDto foodDto)
+        {
+            var na = new Food(ObjectId.Empty, foodDto.name);
+            await foodCollection.InsertOneAsync(na);
+            return CreatedAtRoute(nameof(GetSingleFood), new { Id = na.Id },
+                new FoodDto(na.Id.ToString(), na.Name));
+        }
+
+    }
+}

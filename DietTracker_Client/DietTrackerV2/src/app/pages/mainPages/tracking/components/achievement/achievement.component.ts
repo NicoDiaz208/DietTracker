534f871a1bf8273e0dd4fb6f00dad583362fc36a
Tobias Fraunberger <31957083+Elias278448935@users.noreply.github.com>
2021-06-14 08:38:02 +0200

    Achivemnts Service
    Noch Probleme, weil ionic  noch rot ist warum auch immer

--- DietTracker_Client/DietTrackerV2/src/app/pages/mainPages/tracking/components/achievement/achievement.component.ts
+++ DietTracker_Client/DietTrackerV2/src/app/pages/mainPages/tracking/components/achievement/achievement.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AchievementsService } from 'src/app/services/api/achievements.service';
+import { AchievementDto } from 'src/app/services/model/achievementDto';
 
 @Component({
   selector: 'app-achievement',
@@ -6,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./achievement.component.scss'],
 })
 export class AchievementComponent implements OnInit {
+  public achivements: Array<AchievementDto>;
+ 
+  constructor( private restService: AchievementsService) { }
 
-  constructor() { }
-
-  ngOnInit() {}
+  ngOnInit() {
+    this.restService.apiAchievementsGet().subscribe(data => {this.achivements = (data	as AchievementDto[])});
+  }
 
 }

2fc8a9c99929be07df75a7e58733ed862762f095
Tobias Fraunberger <31957083+Elias278448935@users.noreply.github.com>
2021-06-24 09:30:58 +0200

    Profil anpassung
    Service Profiel

--- DietTracker_Client/DietTrackerV2/src/app/pages/mainPages/profile/profile.component.ts
+++ DietTracker_Client/DietTrackerV2/src/app/pages/mainPages/profile/profile.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { UserDto } from '../../../services/model/userDto';
 import{UserService} from '../../../services/api/user.service';
+import { User } from 'src/app/services/model/user';
+import { DailyProgress } from 'src/app/classes/daily-progress';
+import { DailyProgressDto } from 'src/app/services/model/dailyProgressDto';
+import { DailyProgressService } from 'src/app/services/api/dailyProgress.service';
 
 @Component({
   selector: 'app-profile',
@@ -9,10 +13,14 @@ import{UserService} from '../../../services/api/user.service';
 })
 export class ProfileComponent implements OnInit {
   constructor(private userService: UserService) { }
-  user?:UserDto;
+  public user :User;
+  public progresses: Array<DailyProgressDto>;
 
-  ngOnInit() {
-    this.userService.getSingleUser('60c20ed3be125b81483888d4').subscribe((data)=>{this.user=data;});
+  async ngOnInit() {
+    const ciId = await this.userService.apiUserGetCalorieIntakeByDateGet(localStorage.getItem('clientId'), new Date()).toPromise();
+    this.userService.getSingleUser(ciId).subscribe((data)=>{this.user=(data as User);});
+    this.userService.apiUserGetAllDailyProgressGet(ciId).subscribe((data) =>{this.progresses=(data.reverse() as DailyProgressDto[]);});
+
   }
 
 

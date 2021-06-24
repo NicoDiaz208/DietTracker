43ba003b7d18f2b6c2976f86585e0e842f9b8616
Tobias Fraunberger <31957083+Elias278448935@users.noreply.github.com>
2021-06-24 13:03:17 +0200

    profil

--- DietTracker_Client/DietTrackerV2/src/app/pages/mainPages/profile/profile.component.ts
+++ DietTracker_Client/DietTrackerV2/src/app/pages/mainPages/profile/profile.component.ts
@@ -15,13 +15,17 @@ export class ProfileComponent implements OnInit {
 
   }
   public user :UserDto = {};
-  public progresses: DailyProgress[];
+  public progresses: Array<DailyProgress> = [];
+   year:number = new Date().getFullYear();
+  public age: number;
 
   async ngOnInit() {
     
     this.user = await this.userService.getSingleUser(localStorage.getItem('userId')).toPromise();
-    //this.progresses = await this.userService.apiUserGetAllDailyProgressGet(localStorage.getItem('userId')).toPromise();
+    this.progresses = await this.userService.apiUserGetAllDailyProgressGet(localStorage.getItem('userId')).toPromise();
   }
+
+
 
 
 }

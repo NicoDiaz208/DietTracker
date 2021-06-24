06861045c80bfc03ec448c3fef53b13d156e4509
NicoDiaz208 <nicodiaz208@gmail.com>
2021-06-24 12:35:00 +0200

    Recipe completed v2, Tracking completed

--- DietTracker_Client/DietTrackerV2/src/app/services/api/nutritionFact.service.ts
+++ DietTracker_Client/DietTrackerV2/src/app/services/api/nutritionFact.service.ts
@@ -97,6 +97,44 @@ export class NutritionFactService {
     /**
      * 
      * 
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public apiNutritionFactInitNutritionFactPost(observe?: 'body', reportProgress?: boolean): Observable<NutritionFactCreationDto>;
+    public apiNutritionFactInitNutritionFactPost(observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<NutritionFactCreationDto>>;
+    public apiNutritionFactInitNutritionFactPost(observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<NutritionFactCreationDto>>;
+    public apiNutritionFactInitNutritionFactPost(observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
+
+        let headers = this.defaultHeaders;
+
+        // to determine the Accept header
+        let httpHeaderAccepts: string[] = [
+            'text/plain',
+            'application/json',
+            'text/json'
+        ];
+        const httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        if (httpHeaderAcceptSelected != undefined) {
+            headers = headers.set('Accept', httpHeaderAcceptSelected);
+        }
+
+        // to determine the Content-Type header
+        const consumes: string[] = [
+        ];
+
+        return this.httpClient.request<NutritionFactCreationDto>('post',`${this.basePath}/api/NutritionFact/InitNutritionFact`,
+            {
+                withCredentials: this.configuration.withCredentials,
+                headers: headers,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
+     * 
+     * 
      * @param body 
      * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
      * @param reportProgress flag to report request and response progress.

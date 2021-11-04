f38db2cb6d1a51201c6710bdbe960f87c45f005c
christianangelov <christanangelov@gmail.com>
2021-11-04 05:49:48 +0100

    UserCreate Update

--- DietTracker_Client/DietTrackerV2/src/app/pages/signup/signup.component.ts
+++ DietTracker_Client/DietTrackerV2/src/app/pages/signup/signup.component.ts
@@ -3,6 +3,11 @@ import { Router } from '@angular/router';
 import { FormGroup,FormBuilder,Validators, FormControl } from '@angular/forms';
 import { AlertService } from './../../services/alert/alert.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { LoginService } from 'src/app/services/api/login.service';
+import { UserService } from 'src/app/services/api/user.service';
+import { UserCreationDto } from 'src/app/services/model/userCreationDto';
+import { LoginDto } from 'src/app/services/model/loginDto';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-signup',
@@ -11,15 +16,40 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 })
 export class SignupComponent implements OnInit {
 
+  username = '';
+  password = '';
+  repassword = '';
+  gender = '';
+  dateOfBirth = '';
+  goalWeight = 0;
+  height = 0;
+  email = '';
+  phoneNumber = '';
+  weight = 0;
+  activityLevel =0;
+
+  userCreate: UserCreationDto;
+  loginCreate: LoginDto;
+
   public signupForm: FormGroup;
   constructor(
+    private loginService: LoginService,
+    private userService: UserService,
     private router: Router,
     private readonly fb: FormBuilder,
     ) {
     this.signupForm = this.fb.group({
       username:['', [Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required,Validators.minLength(8)]]
+      password: ['', [Validators.required,Validators.minLength(8)]],
+      repassword: ['', [Validators.required,Validators.minLength(8)]],
+      gender:['', [Validators.required]],
+      dateOfBirth:['', [Validators.required]],
+      goalWeight:['', [Validators.required]],
+      height:['', [Validators.required]],
+      email:['', [Validators.required]],
+      phoneNumber:['', [Validators.required]],
+      activityLevel:['', [Validators.required]],
+      weight:['', [Validators.required]]
     });
   }
   ngOnInit() {}
@@ -34,10 +64,29 @@ export class SignupComponent implements OnInit {
   }
 
 
-  public sumbmit(): void{
-    this.mainnav();
-    const {username,email, password} = this.signupForm.value;
-      console.log('scope is ' + username + email + password);
+  async sumbmit() {
+    if (this.signupForm.get('password').value !== this.signupForm.get('repassword').value) {
+      console.log('Password do not Match');
+    }
+    else{
+      //this.userCreate =
+      //this.user = await this.userService.apiUserPost().toPromise();
+      this.userCreate.name = this.username;
+      let newDate = new Date(this.username);
+      this.userCreate.dateOfBirth = newDate;
+      this.userCreate.gender = this.gender;
+      this.userCreate.goalWeight = this.goalWeight;
+      this.userCreate.height = this.height;
+      this.userCreate.email = this.email;
+      this.userCreate.phoneNumber = this.phoneNumber;
+      this.userCreate.weight = this.weight;
+      this.userCreate.activityLevel = this.activityLevel;
+
+
+      this.mainnav();
+      const {username,email, password} = this.signupForm.value;
+        console.log('scope is ' + username + email + password);
+    }
   }
 
 }

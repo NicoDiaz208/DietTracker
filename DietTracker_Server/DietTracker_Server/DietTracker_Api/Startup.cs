9f83be5cae2028a098bf7ea4efbeec011d8cb99e
NicoDiaz208 <32123786+NicoDiaz208@users.noreply.github.com>
2021-11-04 10:38:03 +0100

    Push Server to cloud configs

--- DietTracker_Server/DietTracker_Server/DietTracker_Api/Startup.cs
+++ DietTracker_Server/DietTracker_Server/DietTracker_Api/Startup.cs
@@ -32,17 +32,17 @@ namespace DietTracker_Api
             services.AddSingleton<CollectionFactory>();
             services.AddControllers();
             services.AddCors();
-            
+
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DietTracker_Api", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DietTrackerApi", Version = "v1" });
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            //if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();

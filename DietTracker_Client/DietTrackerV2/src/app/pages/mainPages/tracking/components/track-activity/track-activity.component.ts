55baac2dce9cc9bc961c1c7c4b8727210260f5a3
Tobias Fraunberger <31957083+Elias278448935@users.noreply.github.com>
2022-01-31 19:02:20 +0100

    Activity
    Activitys erstellen

--- DietTracker_Client/DietTrackerV2/src/app/pages/mainPages/tracking/components/track-activity/track-activity.component.ts
+++ DietTracker_Client/DietTrackerV2/src/app/pages/mainPages/tracking/components/track-activity/track-activity.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivityService } from 'src/app/services/api/activity.service';
 import { UserService } from 'src/app/services/api/user.service';
+import { AcitvityEnum } from 'src/app/services/model/acitvityEnum';
+import { Activity } from 'src/app/services/model/activity';
+import { ActivityCreationDto } from 'src/app/services/model/activityCreationDto';
+import { ActivityDto } from 'src/app/services/model/activityDto';
 import { UserDto } from 'src/app/services/model/userDto';
 
 
@@ -12,14 +16,23 @@ import { UserDto } from 'src/app/services/model/userDto';
 export class TrackActivityComponent implements OnInit {
 
   public user:UserDto = {}
+  public activity: ActivityCreationDto = {}
+  public activities: ActivityDto[] = []
+
 
   constructor(private userService: UserService, private activityService : ActivityService) { 
     
   }
 
-  ngOnInit() {
+  async ngOnInit() {
+    
+    this.activities = await this.activityService.apiActivityGet().toPromise()
+  }
+
+  add(){
     
 
+     this.activityService.apiActivityPost(this.activity)
   }
 
 
